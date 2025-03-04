5f1c9545bf70c4cdb549ca6953223c00f49a035d
unlimitedcodeG <1659159807@qq.com>
2025-03-05 00:34:03 +0800

    feat:首位空格去除双指针

--- main.go
+++ main.go
@@ -1,40 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"log"
 
-	"google.golang.org/protobuf/proto"
-)
 
-type Person struct {
-	Id   int32
-	Name string
-	Tags []string
+import "fmt"
+
+
+func trim(s string) string{
+	if len(s) ==0{
+		return s
+	}
+
+
+	left, right := 0, len(s)-1;
+
+	// 找到第一个非空格字符
+	for left < right&& s[left] == ' '{
+		left ++
+	}
+
+	// 找到最后一个非空格字符
+	for right >=left && s[right] == ' '{
+		right --
+	}
+	return s[left:right+1]
 }
 
-func main() {
-	// 创建对象
-	p := &Person{
-		Id:   123,
-		Name: "Alice",
-		Tags: []string{"gamer", "developer"},
-	}
 
-	// 序列化（编码）
-	data, err := proto.Marshal(p)
-	if err != nil {
-		log.Fatal("序列化失败:", err)
-	}
-
-	fmt.Println("Protobuf 序列化数据:", data)
-
-	// 反序列化（解码）
-	var newPerson Person
-	err = proto.Unmarshal(data, &newPerson)
-	if err != nil {
-		log.Fatal("反序列化失败:", err)
-	}
-
-	fmt.Println("解析后的数据:", newPerson)
+func main(){
+	fmt.Println(trim("  hello world  "))
 }
