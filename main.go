a1cce1cb507451cac7ac5191dc5352d28e091eeb
unlimitedcodeG <1659159807@qq.com>
2025-03-05 02:08:02 +0800

    feat:local_scope

--- main.go
+++ main.go
@@ -1,31 +1,36 @@
 package main
 
-
-
-import "fmt"
-
-
-func trim(s string) string{
-	if len(s) ==0{
+func trim(s string) string {
+	if len(s) == 0 {
 		return s
 	}
 
-
-	left, right := 0, len(s)-1;
+	left, right := 0, len(s)-1
 
 	// 找到第一个非空格字符
-	for left < right&& s[left] == ' '{
-		left ++
+	for left < right && s[left] == ' ' {
+		left++
 	}
 
 	// 找到最后一个非空格字符
-	for right >=left && s[right] == ' '{
-		right --
+	for right >= left && s[right] == ' ' {
+		right--
 	}
-	return s[left:right+1]
+	return s[left : right+1]
 }
 
+var a = "G"
 
-func main(){
-	fmt.Println(trim("  hello world  "))
+func n() { print(a) }
+
+func m() {
+	a := "O"
+	print(a)
+	n()
+}
+
+func main() {
+	n()
+	m()
+	n()
 }
