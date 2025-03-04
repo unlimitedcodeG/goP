f885fed0b91f1045015fde156f47d23d5b5b737e
unlimitedcodeG <1659159807@qq.com>
2025-03-05 04:32:42 +0800

    feat: 验证Ptr

--- ptr.go
+++ ptr.go
@@ -19,7 +19,9 @@ func PtrMain() {
 	fmt.Printf("p:%v\n", p)
 	// const x = 42
 	// ptr := &x // ❌ 编译错误：cannot take the address of x
-
+	var num = 42
+	ptr := &num
+	fmt.Printf("ptr:%v\n", ptr)
 	// const i = 5
 	// fmt.Printf(&i) //invalid operation: cannot take address of i (untyped int constant 5)
 
