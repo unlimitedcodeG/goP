f7cdd4c9d6f74699ba2617743c42d53a822be99f
unlimitedcodeG <1659159807@qq.com>
2025-03-13 06:45:28 +0800

    feat:完成main函数

--- Exchangeapp_backend/main.go
+++ Exchangeapp_backend/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"context"
+	"exchangeapp/config"
+	"exchangeapp/router"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	config.InitConfig()
+
+	r := router.SetupRouter()
+
+	port := config.AppConfig.App.Port
+
+	if port == "" {
+		port = ":8080"
+	}
+
+	srv := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen:%s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+}
