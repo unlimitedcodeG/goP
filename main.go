6d923e707125427eb78b294477deb6a49d130b7b
unlimitedcodeG <1659159807@qq.com>
2025-03-13 09:51:32 +0800

    test:老实结构体

--- main.go
+++ main.go
@@ -1,33 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Circle struct {
 	radius float64
 }
 
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c *Circle) AreaPointer() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
 func main() {
 	var c Circle
-	fmt.Println(c.radius)
-	c.radius = 30
-	fmt.Println(c.getArea())
-	c.changeRadius(20)
-	fmt.Println(c.radius)
-	change(&c, 30)
-	fmt.Println(c.radius)
+	c.radius = 10.00
 
-}
-
-func (c Circle) getArea() float64 {
-	return c.radius * c.radius
-}
-
-// 注意如果想要更改成功c的值，这里需要传指针
-func (c *Circle) changeRadius(radius float64) {
-	c.radius = radius
-}
-
-// 引用类型要想改变值需要传指针
-func change(c *Circle, radius float64) {
-	c.radius = radius
+	// 调用圆形的Area方法
+	fmt.Println("Area of circle:", c.Area())
+	// 调用圆形的AreaPointer方法
+	fmt.Println("Area of circle (pointer):", c.AreaPointer())
 }
