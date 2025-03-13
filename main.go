94188cadb4f58d0b82cd6d9a4597ff3da2c39935
unlimitedcodeG <1659159807@qq.com>
2025-03-13 08:50:57 +0800

    test: callback cb type func

--- main.go
+++ main.go
@@ -2,27 +2,20 @@ package main
 
 import "fmt"
 
-var (
-	myRes = func(a int, b int) int {
-		return a - b
-	}
-)
+// declare type func
+
+type cb func(int) int
 
 func main() {
-	res1 := func(a int, b int) int {
-		return a + b
-	}(10, 25)
+	testCallBack(1, callBack) // testCallBack
 
-	fmt.Printf("res1=%d\n", res1)
+}
 
-	// 匿名函数 付给变量用变量来调用  可多次调用 可是作用地域有限
+func testCallBack(x int, f cb) {
+	f(x)
+}
 
-	res2 := func(a int, b int) int {
-		return a * b
-	}
-	res3 := res2(10, 25)
-	fmt.Printf("res3=%d\n", res3)
-
-	res4 := myRes(10, 25)
-	fmt.Printf("res4=%d\n", res4)
+func callBack(x int) int {
+	fmt.Printf("I am callback x:%d", x)
+	return x
 }
