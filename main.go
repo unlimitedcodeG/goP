62cb70d8b9cca1f8ea6210fe76dc0ade1cabbaa4
unlimitedcodeG <1659159807@qq.com>
2025-03-13 10:59:39 +0800

    feat: 2维数组

--- main.go
+++ main.go
@@ -3,23 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var i, j, k int
+	var value = [3][2]int{{1, 2}, {3, 4}, {5, 6}}
 
-	balance := [5]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
-
-	for i = 0; i < 5; i++ {
-		fmt.Printf("balance[%d] = %f\n", i, balance[i])
-	}
-
-	balance2 := [...]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
-
-	for j = 0; j < 5; j++ {
-		fmt.Printf("balance2[%d] = %f\n", j, balance2[j])
-	}
-
-	balance3 := [5]float32{1: 2.0, 3: 7.0}
-
-	for k = 0; k < 5; k++ {
-		fmt.Printf("balance3[%d] = %f\n", k, balance3[k])
+	for i, v := range value {
+		for j, v2 := range v {
+			fmt.Printf("value[%v][%v]=%v\t", i, j, v2)
+		}
+		fmt.Print(v)
+		fmt.Println()
 	}
 }
