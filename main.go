3a6ef6f76e7689f60c78ac912fd3bf4a2aa62920
unlimitedcodeG <1659159807@qq.com>
2025-03-13 10:35:41 +0800

    test: 快速数组补全

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var n [10]int
-	var i, j int
+	var i, j, k int
 
-	for i = 0; i < 10; i++ {
-		n[i] = i + 100
+	balance := [5]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
+
+	for i = 0; i < 5; i++ {
+		fmt.Printf("balance[%d] = %f\n", i, balance[i])
 	}
 
-	for j = 0; j < 10; j++ {
-		fmt.Printf("e[%d]\n", n[j])
+	balance2 := [...]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
+
+	for j = 0; j < 5; j++ {
+		fmt.Printf("balance2[%d] = %f\n", j, balance2[j])
+	}
+
+	balance3 := [5]float32{1: 2.0, 3: 7.0}
+
+	for k = 0; k < 5; k++ {
+		fmt.Printf("balance3[%d] = %f\n", k, balance3[k])
 	}
 }
