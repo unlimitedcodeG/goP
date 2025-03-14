18f8356defdaf3850a879f8fff3a9c4489366a2e
unlimitedcodeG <1659159807@qq.com>
2025-03-15 03:22:08 +0800

    f:char hello f

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+// var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
 func main() {
-	for i, v := range pow {
-		fmt.Printf("2**%d =%d\n", i, v)
+	for i, c := range "hello" {
+		fmt.Printf("index:%d,char: %c\n", i, c)
 	}
 }
