c59a752f10e792da496b0eac5a28b1271b722c5d
unlimitedcodeG <1659159807@qq.com>
2025-03-14 18:53:07 +0800

    test: Slice Go

--- main.go
+++ main.go
@@ -8,6 +8,14 @@ func main() {
 
 	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
 	printSlice(numbers)
+
+	fmt.Println("numbers ==", numbers)
+
+	fmt.Println("numbers[1:4] ==", numbers[1:4])
+
+	fmt.Println("numbers[:3] ==", numbers[:3])
+
+	fmt.Println("numbers[4:] == ", numbers[4:])
 }
 
 func printSlice(x []int) {
