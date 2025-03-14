8d6c9a1d7bd2df9eae6a01a28ea9da64226e3332
unlimitedcodeG <1659159807@qq.com>
2025-03-14 17:21:41 +0800

    feat:增加numbers

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var value = [3][2]int{{1, 2}, {3, 4}, {5, 6}}
 
-	for i, v := range value {
-		for j, v2 := range v {
-			fmt.Printf("value[%v][%v]=%v\t", i, j, v2)
-		}
-		fmt.Print(v)
-		fmt.Println()
-	}
+	var numbers = make([]int, 3, 5)
+	printSlice(numbers)
+}
+
+func printSlice(x []int) {
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
 }
