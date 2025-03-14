a3a8548254b9dea57551546284362b21c4062a00
unlimitedcodeG <1659159807@qq.com>
2025-03-15 03:53:04 +0800

    f: key &&value

--- main.go
+++ main.go
@@ -5,19 +5,14 @@ import "fmt"
 // var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
 func main() {
-	ch := make(chan int, 2)
-	ch <- 1
-	ch <- 2
-	select {
-	case ch <- 3:
-		fmt.Println("成功发送 3")
-	default:
-		fmt.Println("ch 已经满了 跳过发送 3 ")
-	}
-	close(ch)
+	nums := []int{2, 3, 4}
 
-	for v := range ch {
-		fmt.Println(v)
+	for _, num := range nums {
+		fmt.Println("value:", num)
+	}
+
+	for i := range nums {
+		fmt.Println("index:", i)
 	}
 
 }
