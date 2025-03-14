08f91c79c46988c13577d57796b66ff05713651f
unlimitedcodeG <1659159807@qq.com>
2025-03-15 03:47:59 +0800

    f: send 3

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ func main() {
 	ch := make(chan int, 2)
 	ch <- 1
 	ch <- 2
+	select {
+	case ch <- 3:
+		fmt.Println("成功发送 3")
+	default:
+		fmt.Println("ch 已经满了 跳过发送 3 ")
+	}
+	ch <- 3
 	close(ch)
 
 	for v := range ch {
