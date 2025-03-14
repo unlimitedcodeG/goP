819753a5e3e2b8ea689ea5a0c052d1f969db620b
unlimitedcodeG <1659159807@qq.com>
2025-03-15 03:58:59 +0800

    f:fucky

--- main.go
+++ main.go
@@ -6,13 +6,26 @@ import "fmt"
 
 func main() {
 	nums := []int{2, 3, 4}
+	sum := 0
 
 	for _, num := range nums {
-		fmt.Println("value:", num)
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
 	}
 
-	for i := range nums {
-		fmt.Println("index:", i)
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+
+	for k, v := range kvs {
+		fmt.Printf("%s ->%s\n", k, v)
 	}
 
+	for i, c := range "Go" {
+		fmt.Println(i, c)
+	}
 }
