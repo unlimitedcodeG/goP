631201df50cfcc601cb992a3d473e92f4327cae9
unlimitedcodeG <1659159807@qq.com>
2025-03-15 03:25:36 +0800

    f:key&value

--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import "fmt"
 // var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
 func main() {
-	for i, c := range "hello" {
-		fmt.Printf("index:%d,char: %c\n", i, c)
+
+	map1 := make(map[int]float32)
+
+	map1[1] = 1.0
+
+	for key, value := range map1 {
+		// print key && value
+		fmt.Printf("key is :%d - value is :%f\n", key, value)
 	}
+
+	for key := range map1 {
+		fmt.Printf("key is %d\n", key)
+	}
+
+	for _, value := range map1 {
+		fmt.Printf("value is: %f\n", value)
+	}
+
 }
