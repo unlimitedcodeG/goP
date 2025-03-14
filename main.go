71bc6a44e6dbaa4ba9e79c7bbba968cc8488f343
unlimitedcodeG <1659159807@qq.com>
2025-03-15 04:39:25 +0800

    f:ptr

--- main.go
+++ main.go
@@ -2,25 +2,8 @@ package main
 
 import "fmt"
 
-// var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
-
 func main() {
-	countryCapitalMap := map[string]string{
-		"France": "Paris",
-		"Italy":  "Rome",
-		"Japan":  "Tokyo",
-		"India":  "New delhi",
-	}
-	fmt.Println("origin map")
+	var a int = 10
 
-	for country := range countryCapitalMap {
-		fmt.Println(country, "首都是", countryCapitalMap[country])
-
-		delete(countryCapitalMap, "France")
-		fmt.Println("delete France")
-
-		for country := range countryCapitalMap {
-			fmt.Println(country, "首都是", countryCapitalMap[country])
-		}
-	}
+	fmt.Printf("变量的地址: %x\n", &a)
 }
