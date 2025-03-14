0464a5b9b0b9263d29e1367645ee7abe7b4fdc0f
unlimitedcodeG <1659159807@qq.com>
2025-03-15 03:42:32 +0800

    f: for ch

--- main.go
+++ main.go
@@ -5,22 +5,13 @@ import "fmt"
 // var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
 func main() {
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	close(ch)
 
-	map1 := make(map[int]float32)
-
-	map1[1] = 1.0
-
-	for key, value := range map1 {
-		// print key && value
-		fmt.Printf("key is :%d - value is :%f\n", key, value)
-	}
-
-	for key := range map1 {
-		fmt.Printf("key is %d\n", key)
-	}
-
-	for _, value := range map1 {
-		fmt.Printf("value is: %f\n", value)
+	for v := range ch {
+		fmt.Println(v)
 	}
 
 }
