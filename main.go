efd99aface64ab8d3cee61135edb4acd3cba0359
unlimitedcodeG <1659159807@qq.com>
2025-03-16 14:36:53 +0800

    f:array

--- main.go
+++ main.go
@@ -1,52 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	i := 2
 
-	fmt.Print("Write ", i, " as ")
+	var a [5]int
+	fmt.Println("emp:", a)
 
-	switch i {
-	case 1:
-		fmt.Println("one")
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
 
-	case 2:
-		fmt.Println("two")
-	case 3:
-		fmt.Println("three")
-	}
+	fmt.Println("len:", len(a))
 
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("It is the weekend")
-	default:
-		fmt.Println("It's a weekday")
-	}
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
 
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("It's before noon")
-	default:
-		fmt.Println("It's after noon")
-	}
+	b = [...]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
 
-	whatAmI := func(i interface{}) {
-		switch t := i.(type) {
-		case bool:
-			fmt.Println("I'm a bool")
-		case int:
-			fmt.Println("I'm an int")
-		default:
-			fmt.Printf("Don't know type %T\n", t)
+	b = [...]int{100, 3: 400, 500}
+	fmt.Println("idx:", b)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
 		}
 	}
+	fmt.Println("2d: ", twoD)
 
-	whatAmI(true)
-	whatAmI(1)
-	whatAmI("hey")
+	twoD = [2][3]int{
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	fmt.Println("2d: ", twoD)
 }
