5bfd5c7f4b559461fe3affd6b488e8d17509bfe1
unlimitedcodeG <1659159807@qq.com>
2025-03-18 07:41:21 +0800

    f:fib

--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import "fmt"
 
-func intSeq() func() int {
-	i := 0
-	return func() int {
-		i++
-		return i
+func fact(n int) int {
+	if n == 0 {
+		return 1
 	}
+	return n * fact(n-1)
 }
 
 func main() {
+	fmt.Println(fact(7))
+	var fib func(n int) int
 
-	nextInt := intSeq()
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
 
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-
-	newInts := intSeq()
-	fmt.Println(newInts())
+		return fib(n-1) + fib(n-2)
+	}
 }
