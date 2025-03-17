93f6f68f2a6e77ad9bd980a85007314de9ca5c7d
unlimitedcodeG <1659159807@qq.com>
2025-03-18 07:50:09 +0800

    f:built-in main.go

--- main.go
+++ main.go
@@ -2,22 +2,31 @@ package main
 
 import "fmt"
 
-func fact(n int) int {
-	if n == 0 {
-		return 1
-	}
-	return n * fact(n-1)
-}
-
 func main() {
-	fmt.Println(fact(7))
-	var fib func(n int) int
 
-	fib = func(n int) int {
-		if n < 2 {
-			return n
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
 		}
+	}
 
-		return fib(n-1) + fib(n-2)
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
 	}
 }
