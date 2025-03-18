04f83fed9103cd0381636ac9fa42f1278d692bd3
unlimitedcodeG <1659159807@qq.com>
2025-03-18 08:09:18 +0800

    f:ptr

--- main.go
+++ main.go
@@ -2,31 +2,23 @@ package main
 
 import "fmt"
 
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
 func main() {
+	i := 1
+	fmt.Println("initial:", i)
 
-	nums := []int{2, 3, 4}
-	sum := 0
-	for _, num := range nums {
-		sum += num
-	}
-	fmt.Println("sum:", sum)
+	zeroval(i)
+	fmt.Println("zeroval:", i)
 
-	for i, num := range nums {
-		if num == 3 {
-			fmt.Println("index:", i)
-		}
-	}
+	zeroptr(&i)
+	fmt.Println("zeroptr:", i)
 
-	kvs := map[string]string{"a": "apple", "b": "banana"}
-	for k, v := range kvs {
-		fmt.Printf("%s -> %s\n", k, v)
-	}
-
-	for k := range kvs {
-		fmt.Println("key:", k)
-	}
-
-	for i, c := range "go" {
-		fmt.Println(i, c)
-	}
+	fmt.Println("pointer:", &i)
 }
