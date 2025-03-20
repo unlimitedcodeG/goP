4eacd0e103229514f952b843efd591922bb5be73
unlimitedcodeG <1659159807@qq.com>
2025-03-20 13:04:23 +0800

    f:go

--- main.go
+++ main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"sync"
 )
 
+func f(from string, wg *sync.WaitGroup) {
+	defer wg.Done() // 任务完成时减少计数
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
 func main() {
-	fmt.Println("CPU 核心数:", runtime.NumCPU())
+	var wg sync.WaitGroup
+
+	wg.Add(2) // 等待两个 Goroutine 完成
+
+	go f("goroutine", &wg)
+	go func(msg string) {
+		fmt.Println(msg)
+		wg.Done()
+	}("going")
+
+	wg.Wait() // 等待所有 Goroutine 结束
+	fmt.Println("done")
 }
