4583eeb94227d1d93b6686a1311a687cb10627ff
unlimitedcodeG <1659159807@qq.com>
2025-03-20 09:24:51 +0800

    go：goroutine

--- main.go
+++ main.go
@@ -1,35 +1,26 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"time"
 )
 
-type argError struct {
-	arg     int
-	message string
-}
-
-func (e *argError) Error() string {
-	return fmt.Sprintf("%d - %s", e.arg, e.message)
-}
-
-func f(arg int) (int, error) {
-	if arg == 42 {
-
-		return -1, &argError{arg, "can't work with it"}
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
 	}
-	return arg + 3, nil
 }
 
 func main() {
 
-	_, err := f(42)
-	var ae *argError
-	if errors.As(err, &ae) {
-		fmt.Println(ae.arg)
-		fmt.Println(ae.message)
-	} else {
-		fmt.Println("err doesn't match argError")
-	}
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
 }
